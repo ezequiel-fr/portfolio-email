bce3690510fb3acf50631621eafee7910766438f
Ezequiel Dev <theredminedu51@gmail.com>
2025-06-14 20:24:01 +0200

    Send acknowledgment and update the error messages

--- src/pages/email.php
+++ src/pages/email.php
@@ -19,11 +19,25 @@ try {
         $_POST[$var] = trim(htmlspecialchars_decode($_POST[$var]));
     }
 
+    // Send an acknowledgment email to the sender before sending the main email
+    send_mail(
+        $_POST['email'],
+        $_POST['name'],
+        'Merci pour votre message',
+        file_get_contents(__DIR__.'/../../templates/acknowledgment.html'),
+        [
+            'subject' => $_POST['subject'],
+            'message' => nl2br(substr($_POST['message'], 0, 300)) . (
+                strlen($_POST['message']) > 300 ? '...' : ''
+            )
+        ],
+    );
+
     // Send the email
     send_mail(
         $_ENV['SAMPLE_RECEIVER_MAIL'] ?? $_ENV['CONTACT_MAIL'],
         $_ENV['SAMPLE_RECEIVER_NAME'] ?? $_ENV['CONTACT_NAME'],
-        'Contact from portfolio',
+        'Nouveau message depuis le portfolio',
         file_get_contents(__DIR__.'/../../templates/email.html'),
         [
             'name' => $_POST['name'],
@@ -36,11 +50,13 @@ try {
     echo json_encode([
         'status' => 'success',
         'message' => 'Email sent successfully!',
+        'messageCode' => 'success.email_sent',
     ]);
 } catch (PHPMailer\Exception $e) {
     echo json_encode([
         'status' => 'error',
         'message' => 'Failed to send email: '.$e->getMessage(),
+        'messageCode' => 'error.not_found',
     ]);
 
     $code = 500;
@@ -48,6 +64,7 @@ try {
     echo json_encode([
         'status' => 'error',
         'message' => 'An error occurred: '.$th->getMessage(),
+        'messageCode' => 'error.internal_server_error',
     ]);
 
     $code = 500;
